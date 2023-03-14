ff24a53f53d71f9cbce34926b2e20603023ba74d
sgsgsguo <113867248+sgsgsguo@users.noreply.github.com>
2023-03-13 22:25:46 -0700

    Update MarkovWordOne.java

--- WordNGram/MarkovWordOne.java
+++ WordNGram/MarkovWordOne.java
@@ -23,15 +23,61 @@ public class MarkovWordOne implements IMarkovModel {
     public void setTraining(String text){
 		myText = text.split("\\s+");
 	}
+    
+    private int indexOf(String[] words, String target, int start) {
+    	for (int i =start; i < words.length; i++) {    		
+    		if (words[i].equals(target)) {
+    			return i;
+    		}
+    	}    	
+		return -1;
+    }
+    
+    public void testIndexOf() {    	
+    	String text = "this is just a test yes this is a simple test";
+    	String[] words = text.split("\\s+");
+    	System.out.println(indexOf(words, "this", 0));
+    	System.out.println(indexOf(words, "this", 3));
+    	System.out.println(indexOf(words, "frog", 0));
+    	System.out.println(indexOf(words, "frog", 5));
+    	System.out.println(indexOf(words, "simple", 2));
+    	System.out.println(indexOf(words, "test", 5));
+    }
+    
+    
+    private ArrayList<String> getFollows(String key) {
+	    ArrayList<String> follows = new ArrayList<String>();
+	    int index =-1;	
+	    for (int pos =0; pos < myText.length-1; pos++) {	    
+	    	index = indexOf(myText, key, pos);
+	    	if (index < 0 || index >= myText.length-1) {
+	    		break;
+	    	}
+	    	follows.add(myText[index+1]);   
+	    	pos = index +1;	    	
+	    }	    
+	    return follows;
+    }
+    
+    public void getFollowsTest() {    	
+    	System.out.println(getFollows("test"));
+    	System.out.println(getFollows("is"));
+    }
 	
 	public String getRandomText(int numWords){
 		StringBuilder sb = new StringBuilder();
-		int index = myRandom.nextInt(myText.length-1);  // random word to start with
+		
+		// random word to start with
+		int index = myRandom.nextInt(myText.length-1);  
+		
 		String key = myText[index];
+		System.out.println(key);
 		sb.append(key);
 		sb.append(" ");
+		System.out.println(sb);
 		for(int k=0; k < numWords-1; k++){
 		    ArrayList<String> follows = getFollows(key);
+		    System.out.println(follows);
 		    if (follows.size() == 0) {
 		        break;
 		    }
@@ -40,14 +86,11 @@ public class MarkovWordOne implements IMarkovModel {
 			sb.append(next);
 			sb.append(" ");
 			key = next;
-		}
-		
+		}		
 		return sb.toString().trim();
 	}
 	
-	private ArrayList<String> getFollows(String key) {
-	    ArrayList<String> follows = new ArrayList<String>();
-	    return follows;
-    }
-
+	public String toString() {
+		return "MarkovWordOne";
+	}	
 }
